f5990d1ed73e4503066e002a9c3966922b479144
Matias Nicolas Michelli <proga2332@gmail.com>
2022-09-26 00:04:50 -0300

    add url deployment rick and morty proyect

--- src/app/pages/proyects/proyects.component.ts
+++ src/app/pages/proyects/proyects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../../shared/interfaces/menu.interface';
 
 @Component({
   selector: 'app-proyects',
@@ -6,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyects.component.css']
 })
 export class ProyectsComponent implements OnInit {
+
+  public proyectInformation: MenuItem[] = [
+    {
+      text:'Rick and Morty',
+      path: 'https://morty-app.netlify.app/'
+    },
+    {
+      text:'Profile Web',
+      path: 'https://matiasprofile.netlify.app/'
+    },
+    {
+      text: 'Medical turner',
+      path: 'https://calculador-peso-corporal.netlify.app/'
+    },
+    {
+      text: 'Real estate',
+      path: 'https://matute693.github.io/Bienes-Raices/'
+    },
+    {
+      text: 'Task list',
+      path: 'https://app-tarea.netlify.app/'
+    },
+    {
+      text: 'Trips',
+      path: 'https://viajesmundo.netlify.app'
+    }
+  ]
 
   constructor() { }
 
